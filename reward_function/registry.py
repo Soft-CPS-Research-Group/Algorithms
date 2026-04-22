36b6ec1bafbf18f141826d604479a29160b32c0a
Pedro Monteiro <pedro.monteiro@uphold.com>
2026-04-23 00:05:38 +0100

    Introduction of custom reward function

--- reward_function/registry.py
+++ reward_function/registry.py
@@ -7,11 +7,13 @@ from typing import Dict, Type
 from citylearn.reward_function import RewardFunction
 
 from reward_function.V2G_Reward import V2GPenaltyReward
+from reward_function.cc_reward import CCReward
 from reward_function.cost_hard_constraint_reward import CostHardConstraintReward
 from reward_function.cost_minimization_reward import CostMinimizationReward
 
 REWARD_FUNCTION_MAP: Dict[str, Type[RewardFunction]] = {
     "RewardFunction": RewardFunction,
+    "CCReward": CCReward,
     "V2GPenaltyReward": V2GPenaltyReward,
     "CostMinimizationReward": CostMinimizationReward,
     "CostHardConstraintReward": CostHardConstraintReward,
